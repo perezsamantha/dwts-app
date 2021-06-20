8fb5de46edd017ae97badd5d354ed8cb2b49a790
Samantha Perez <samanthaaaperezzz@gmail.com>
2021-06-20 00:42:53 -0400

    signin works from frontend for now?

--- dwts-app/client/src/actions/auth.js
+++ dwts-app/client/src/actions/auth.js
@@ -7,7 +7,7 @@ export const signin = (formData, history) => async (dispatch) => {
 
         dispatch({ type: AUTH, data });
 
-        history.push('/');
+        //history.push('/');
     } catch (error) {
         console.log(error);
     }
