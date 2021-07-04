8bc0286da8fa17385be7620cd91cb99b52c5391a
Samantha Perez <samanthaaaperezzz@gmail.com>
2021-07-04 17:30:01 -0400

    small typo fix

--- dwts-app/client/src/actions/auth.js
+++ dwts-app/client/src/actions/auth.js
@@ -13,7 +13,7 @@ export const signin = (formData, history) => async (dispatch) => {
     }
 }
 
-export const singup = (formData, history) => async (dispatch) => {
+export const signup = (formData, history) => async (dispatch) => {
     try {
         const { data } = await api.signUp(formData);
 
