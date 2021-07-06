7aa39f9bcc3c7f8493ae2cb3ff5046e3a1436dab
Samantha Perez <samanthaaaperezzz@gmail.com>
2021-07-06 00:12:04 -0400

    navbar now sticky to bottom, fixed padding issue

--- dwts-app/client/src/components/Dashboard/Dashboard.js
+++ dwts-app/client/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction, makeStyles } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
+const useStyles = makeStyles({
+    stickToBottom: {
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
+        //padding: '0 1em'
+    }
+})
+
 function Dashboard() {
+    const classes = useStyles();
     const [value, setValue] = useState('home');
 
     const handleChange = (event, newValue) => {
@@ -15,7 +25,7 @@ function Dashboard() {
     return (
         <div>
             <h2>Dashboard</h2>
-            <BottomNavigation value={value} onChange={handleChange}>
+            <BottomNavigation value={value} onChange={handleChange} className={classes.stickToBottom}>
                 <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
                 <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
                 <BottomNavigationAction label="Search" value="search" icon={<SearchIcon />} />
