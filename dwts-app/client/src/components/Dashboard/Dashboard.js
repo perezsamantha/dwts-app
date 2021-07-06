dbe68da7136a341d13a3a7cf8c15af196109bfad
Samantha Perez <samanthaaaperezzz@gmail.com>
2021-07-05 23:55:57 -0400

    added non-functional navbar to dashboard page

--- dwts-app/client/src/components/Dashboard/Dashboard.js
+++ dwts-app/client/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Dashboard() {
+    const [value, setValue] = useState('home');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    }
+
     return (
-        <h2>Dashboard</h2>
+        <div>
+            <h2>Dashboard</h2>
+            <BottomNavigation value={value} onChange={handleChange}>
+                <BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} />
+                <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+                <BottomNavigationAction label="Search" value="search" icon={<SearchIcon />} />
+                <BottomNavigationAction label="Account" value="account" icon={<AccountCircleIcon />} />
+            </BottomNavigation>
+        </div>
     );
 }
 
