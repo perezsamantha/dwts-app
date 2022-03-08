d80ce3af60add8dfeab9910f08714a431579c61f
Samantha Perez <samanthaaaperezzz@gmail.com>
2022-03-07 22:21:29 -0500

    restyled bottom nav bar

--- dwts-app/client/src/components/BottomNavBar/BottomNavBar.js
+++ dwts-app/client/src/components/BottomNavBar/BottomNavBar.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
-import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
-//import NotificationsIcon from '@mui/icons-material/Notifications';
-//import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import { Link } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
 import styled from '@emotion/styled';
+
+import { FiSearch, FiBarChart2 } from 'react-icons/fi';
+
+import { AiOutlineUser } from 'react-icons/ai';
+
+import { IoMdNotificationsOutline } from 'react-icons/io';
+
+import { BsStars } from 'react-icons/bs';
 
 function BottomNavBar() {
     //const pathname = window.location.pathname;
@@ -20,7 +22,8 @@ function BottomNavBar() {
         value !== 'dashboard' &&
         value !== 'overview' &&
         value !== 'search' &&
-        value !== 'account'
+        value !== 'account' &&
+        value !== 'notifications'
     ) {
         setValue('home');
     }
@@ -33,44 +36,43 @@ function BottomNavBar() {
         <NavBarContainer elevation={4}>
             <BottomNavigation value={value} onChange={handleChange}>
                 <BottomNavigationAction
-                    disableRipple
                     component={Link}
                     to="/dashboard"
                     //label="Home"
                     value="dashboard"
-                    icon={<HomeIcon />}
+                    icon={<CustomHomeIcon />}
                 />
                 <BottomNavigationAction
-                    disableRipple
                     component={Link}
                     to="/overview"
                     //label="Overview"
                     value="overview"
-                    icon={<LeaderboardIcon />}
+                    icon={<CustomOverviewIcon />}
                 />
                 <BottomNavigationAction
-                    disableRipple
                     component={Link}
-                    to="/search"
+                    to="/search/dances"
                     //label="Search"
                     value="search"
-                    icon={<SearchIcon />}
+                    icon={
+                        <CustomSearch>
+                            <CustomSearchIcon />
+                        </CustomSearch>
+                    }
                 />
-                {/* <BottomNavigationAction
-                    disableRipple
+                <BottomNavigationAction
                     component={Link}
                     to="/notifications"
                     //label="Search"
-                    value="search"
-                    icon={<NotificationsIcon />}
-                /> */}
+                    value="notifications"
+                    icon={<CustonNotificationsIcon />}
+                />
                 <BottomNavigationAction
-                    disableRipple
                     component={Link}
                     to="/account"
                     //label="Account"
                     value="account"
-                    icon={<AccountBoxIcon />}
+                    icon={<CustomAccountIcon />}
                 />
             </BottomNavigation>
         </NavBarContainer>
@@ -83,5 +85,50 @@ const NavBarContainer = styled(Paper)`
     width: 100%;
     z-index: 10;
 `;
+
+const CustomSearch = styled(Paper)(({ theme }) => ({
+    width: 50,
+    height: 50,
+    marginTop: -50,
+    borderRadius: '50%',
+    backgroundColor:
+        theme.palette.mode === 'dark'
+            ? theme.palette.primary.dark
+            : theme.palette.primary.main,
+    boxShadow: `0 5px 15px ${theme.palette.primary.main}`,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}));
+
+const CustomSearchIcon = styled(FiSearch)(({ theme }) => ({
+    width: 30,
+    height: 30,
+    color: theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.7)' : 'white',
+}));
+
+const CustomHomeIcon = styled(BsStars)({
+    width: 25,
+    height: 25,
+    marginTop: -5,
+});
+
+const CustomOverviewIcon = styled(FiBarChart2)({
+    width: 25,
+    height: 25,
+    marginTop: -5,
+});
+
+const CustonNotificationsIcon = styled(IoMdNotificationsOutline)({
+    width: 25,
+    height: 25,
+    marginTop: -5,
+});
+
+const CustomAccountIcon = styled(AiOutlineUser)({
+    width: 25,
+    height: 25,
+    marginTop: -5,
+});
 
 export default BottomNavBar;
